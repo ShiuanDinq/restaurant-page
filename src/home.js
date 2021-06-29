3783ef0ee8c1e1eba4d6464d3b4efc4a273ec0b8
edmund-hsd <edmundhang@ymail.com>
2021-06-29 15:56:16 +0700

    restructure index.js add clear.js home.js nav.js
    website now able to swith between pages

---
+++ src/home.js
@@ -0,0 +1,33 @@
+import nav from './nav.js';
+
+let home = () => {
+
+  let content = document.querySelector('#content');
+  content.classList.add('home');
+
+  //insert nav
+  if(!document.querySelector('nav')) {
+    let navbar = nav();
+    content.appendChild(navbar);
+  }
+
+  //container
+  let container = document.createElement('div');
+  container.classList.add('container');
+  content.appendChild(container);
+  
+
+  //header
+  let header = document.createElement('header');
+  container.appendChild(header);
+  let headline = document.createElement('h1');
+  headline.textContent = `Cik Wee's Chwee Kueh`;
+  header.appendChild(headline);
+  let tagline = document.createElement('p');
+  tagline.innerHTML = 'Home of the best Chwee Kueh';
+  header.appendChild(tagline);
+
+
+}
+
+export default home
--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log('testingfdfdfd')
+import './style.css';
+import home from './home.js';
+import clear from './clear.js';
+
+
+
+home();
+
+
+
+let homeBtn = document.getElementById('home');
+homeBtn.onclick = () => {
+  clear();
+  home();
+};
